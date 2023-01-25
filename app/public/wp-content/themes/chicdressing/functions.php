3464c5fd4f7868795f5d6bffbfd5c29e362d7db0
RomainG-A <110917325+RomainG-A@users.noreply.github.com>
2023-01-25 22:30:27 +0100

    corrections on product titles

--- app/public/wp-content/themes/chicdressing/functions.php
+++ app/public/wp-content/themes/chicdressing/functions.php
@@ -7,7 +7,13 @@ function chicdressing_enqueue_styles() {
 
 add_filter( 'big_image_size_threshold', '__return_false' );
 
+/*
+    Fonctions de Romain
+*/
 
-
-
- 
+// h2 => h3 sur les titres de produits
+remove_action( 'woocommerce_shop_loop_item_title','woocommerce_template_loop_product_title', 10 );
+add_action('woocommerce_shop_loop_item_title', 'chicdressing_woo_shop_products_title', 10 );
+function chicdressing_woo_shop_products_title() {
+    echo '<h3 class="' . esc_attr( apply_filters( 'woocommerce_product_loop_title_classes', 'woocommerce-loop-product__title' ) ) . '">' . get_the_title() . '</h3>'; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+}
